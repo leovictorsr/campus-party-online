eb7e752e808c9ffc0849c4b1bdbbc889072af63d
Tony K <fda.andre@gmail.com>
2016-11-22 14:05:18 -0200

    add priorities-table

--- src/interest_list.js
+++ src/interest_list.js
@@ -1,12 +1,4 @@
-function go_to_avatar_part_1()
-{
-    window.open("./avatar-part-1.html", "_self");
-}
-
-function go_to_avatar_part_3()
-{
-    window.open("./avatar-part-3.html", "_self");
-}
+'use strict';
 
 var areas = []
 
