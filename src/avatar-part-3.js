e72b610606e6176d325179d342a9fcddbdc47319
Tony K <fda.andre@gmail.com>
2016-11-22 17:43:22 -0200

    Enable mission selection

--- src/avatar-part-3.js
+++ src/avatar-part-3.js
@@ -7,3 +7,25 @@ function go_to_main_screen()
 {
     window.open("./menu-main.html", "_self");
 }
+
+function missionClicked(num){
+    if (mission[num] == 0){
+        mission[num] = 1;
+        document.getElementById("mission" + num).style.backgroundColor = "#003A69";
+        document.getElementById("mission" + num).style.color = "white";
+        document.getElementById("missionb" + num).innerHTML = "x";
+        document.getElementById("missionb" + num).style.backgroundColor = "red";
+    } else {
+        mission[num] = 0;
+        document.getElementById("mission" + num).style.backgroundColor = "#00C4D9";
+        document.getElementById("mission" + num).style.color = "black";
+        document.getElementById("missionb" + num).innerHTML = "+";
+        document.getElementById("missionb" + num).style.backgroundColor = "green";
+    }
+}
+
+var mission = []
+var i = 0;
+
+while(i < 9)
+    mission[i++] = 0;
